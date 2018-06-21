a4696eecef991fc7d12f6c2a0062ccd7587f8aa5
David Guerrero <davidguerreromejias@gmail.com>
2018-06-21 12:14:47 +0200

    Create, list and download a bucket in S3.

--- Lambda-TEST/src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
+++ Lambda-TEST/src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -1,12 +1,25 @@
 package com.amazonaws.lambda.demo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.UUID;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.Bucket;
 
 public class LambdaFunctionHandler implements RequestHandler<S3Event, String> {
 
@@ -22,21 +35,108 @@ public class LambdaFunctionHandler implements RequestHandler<S3Event, String> {
     @Override
     public String handleRequest(S3Event event, Context context) {
         context.getLogger().log("Received event: " + event);
+        
+        String bucketName = "Test-s3-bucket-" + UUID.randomUUID();
 
         // Get the object from the event and show its content type
         String bucket = event.getRecords().get(0).getS3().getBucket().getName();
         String key = event.getRecords().get(0).getS3().getObject().getKey();
         try {
-            S3Object response = s3.getObject(new GetObjectRequest(bucket, key));
-            String contentType = response.getObjectMetadata().getContentType();
-            context.getLogger().log("CONTENT TYPE: " + contentType);
+        	
+            /*
+             * Create a new S3 bucket - Amazon S3 bucket names are globally unique,
+             * so once a bucket name has been taken by any user, you can't create
+             * another bucket with that same name.
+             *
+             * You can optionally specify a location for your bucket if you want to
+             * keep your data closer to your applications or users.
+             */
+            System.out.println("Creating bucket " + bucketName + "\n");
+            s3.createBucket(bucketName);
+            
+            /*
+             * List the buckets in your account
+             */
+            System.out.println("Listing buckets");
+            for (Bucket b : s3.listBuckets()) {
+                System.out.println(" - " + b.getName());
+            }
+            System.out.println();
+            
+            /*
+             * Upload an object to your bucket - You can easily upload a file to
+             * S3, or upload directly an InputStream if you know the length of
+             * the data in the stream. You can also specify your own metadata
+             * when uploading to S3, which allows you set a variety of options
+             * like content-type and content-encoding, plus additional metadata
+             * specific to your applications.
+             */
+            System.out.println("Uploading a new object to S3 from a file\n");
+            s3.putObject(new PutObjectRequest(bucketName, key, createRandomFile()));
+            
+            /*
+             * Download an object - When you download an object, you get all of
+             * the object's metadata and a stream from which to read the contents.
+             * It's important to read the contents of the stream as quickly as
+             * possibly since the data is streamed directly from Amazon S3 and your
+             * network connection will remain open until you read all the data or
+             * close the input stream.
+             *
+             * GetObjectRequest also supports several other options, including
+             * conditional downloading of objects based on modification times,
+             * ETags, and selectively downloading a range of an object.
+             */
+            System.out.println("Downloading an object");
+            S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));
+            String contentType = object.getObjectMetadata().getContentType();
+            System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
+            readTextObject(object.getObjectContent());
+        	
+
             return contentType;
         } catch (Exception e) {
             e.printStackTrace();
             context.getLogger().log(String.format(
                 "Error getting object %s from bucket %s. Make sure they exist and"
                 + " your bucket is in the same region as this function.", key, bucket));
-            throw e;
+            try {
+				throw e;
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
         }
+		return key;
+    }
+    
+    private void readTextObject(S3ObjectInputStream objectContent) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(objectContent));
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) break;
+            System.out.println("    " + line);
+        }
+        System.out.println();
+	}
+
+	private static File createRandomFile() throws IOException {
+    	File file = File.createTempFile("aws-java-test", ".txt");
+        file.deleteOnExit();
+
+        Writer writer = new OutputStreamWriter(new FileOutputStream(file));
+        writer.write("qwertyuiopasdfghjklñzxcvbnm\n");
+        writer.write("01234567890112345678901234\n");
+        writer.write("!@#$%^&*()-=[]{};':',.<>/?\n");
+        writer.write("qwertyuiopasdfghjklñzxcvbnm\n");
+        writer.write("01234567890112345678901234\n");
+        writer.write("!@#$%^&*()-=[]{};':',.<>/?\n");
+        writer.write("qwertyuiopasdfghjklñzxcvbnm\n");
+        writer.write("01234567890112345678901234\n");
+        writer.write("!@#$%^&*()-=[]{};':',.<>/?\n");
+        writer.write("qwertyuiopasdfghjklñzxcvbnm\n");
+        writer.write("01234567890112345678901234\n");
+        writer.write("!@#$%^&*()-=[]{};':',.<>/?\n");
+        writer.close();
+    	return file;
     }
 }
